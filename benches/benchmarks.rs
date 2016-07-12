4f29150e09a15dd70dd5285de28efe3b620f3879
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2016-07-12 13:06:58 +0100

    Use 100k lat, lon pairs in benchmark

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -4,10 +4,18 @@ use test::Bencher;
 extern crate polyline;
 use polyline::encode_coordinates;
 
+extern crate rand;
+use rand::distributions::{IndependentSample, Range};
+
 #[bench]
 #[allow(unused_must_use)]
 fn bench_threads(b: &mut Bencher) {
-    let res = vec![[38.5, -120.2], [40.7, -120.95], [430.252, -126.453]];
+    let num_coords = 10000;
+    // These coordinates cover London, approximately
+    let between_lon = Range::new(-6.379880, 1.768960);
+    let between_lat = Range::new(49.871159, 55.811741);
+    let mut rng = rand::thread_rng();
+    let res = vec![[between_lat.ind_sample(&mut rng), between_lon.ind_sample(&mut rng)]; num_coords];
     b.iter(||{
         encode_coordinates(&res, 5);
     });
