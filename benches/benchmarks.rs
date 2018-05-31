bbfef48ce1796170f6d8bf6a4d159f9e8630b56b
Stephan Hügel <urschrei@gmail.com>
2018-05-31 17:58:42 +0100

    Replace deprecated sampling method in benchmark

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -5,7 +5,7 @@ extern crate polyline;
 use polyline::encode_coordinates;
 
 extern crate rand;
-use rand::distributions::{IndependentSample, Range};
+use rand::distributions::{Distribution, Range};
 
 #[bench]
 #[allow(unused_must_use)]
@@ -15,7 +15,7 @@ fn bench_threads(b: &mut Bencher) {
     let between_lon = Range::new(-6.379880, 1.768960);
     let between_lat = Range::new(49.871159, 55.811741);
     let mut rng = rand::thread_rng();
-    let res = vec![[between_lat.ind_sample(&mut rng), between_lon.ind_sample(&mut rng)]; num_coords];
+    let res = vec![[between_lat.sample(&mut rng), between_lon.sample(&mut rng)]; num_coords];
     b.iter(||{
         encode_coordinates(&res, 5);
     });
